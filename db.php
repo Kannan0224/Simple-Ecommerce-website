71c9bf0540701cde07f4efedcaa6a5f392bf731a
KANNAN <kannansang246@gmail.com>
2020-12-21 22:34:07 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,16 @@
-<?php
-$con=mysqli_connect("sql310.epizy.com","epiz_26774798","Kannansoni","epiz_26774798_kannan");
-//$con=mysqli_connect("localhost","root","","kannan");
-if(!$con)
- {
-    echo "not connected".mysqli_error($con);
- }
-else
- {}
+<?php
+$con=mysqli_connect("localhost","root","","kannan");
+
+if(!$con)
+
+ {
+
+    echo "not connected".mysqli_error($con);
+
+ }
+
+else
+
+ {}
+
 ?>
